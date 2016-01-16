6da5233ff905a99346908142f465bf3bc2db8c63
Kevin Zhang <mobilecheng@qq.com>
2016-01-16 16:46:23 +0800

    Initial Commit

---
+++ LoveInNZ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoveInNZ
+//
+//  Created by Kevin Zhang on 16/1/16.
+//  Copyright © 2016年 BearMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
